c08b00dba3e82fdec5707384c52ce24d539eaf17
Manimall <gorpunov94@mail.ru>
2019-09-26 15:11:33 +0300

    -updateItems

--- src/modules/Shared/sagas.js
+++ src/modules/Shared/sagas.js
@@ -1,16 +1,16 @@
 import { takeLatest, fork, put, call } from 'redux-saga/effects';
 import { fetchMoviesRequest, fetchMoviesSuccess, fetchMoviesFailure } from "../Movies";
-import {getMovies} from "../Movies/api";
-import { updateItemsRequest, updateItemsSuccess } from "./actions";
+import { getMovies } from "../Movies/api";
+import { updateItems } from "./actions";
 
 
 function* fetchMoviesUpdateFlow ({payload}) {
 	try {
 		console.log(payload);
 		const result = yield call(getMovies, payload);
-		console.log(result);
-		// yield put(fetchMoviesSuccess(result));
-		yield put(updateItemsSuccess(result));
+		// console.log(result);
+		yield put(fetchMoviesSuccess(result));
+		yield put(updateItems(result));
 	} catch (error) {
 		console.log(error);
 		yield put(fetchMoviesFailure(error));
